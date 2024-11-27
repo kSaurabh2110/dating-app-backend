330cd06ef491faf93b486ef2e297c8ee6806f789
Saurabh Kumar <s.kumar.21101999@gmail.com>
2024-11-28 00:30:54 +0530

    Initilise spring project

---
+++ userFacing/src/main/java/com/soulstar/userFacing/UserFacingApplication.java
@@ -0,0 +1,13 @@
+package com.soulstar.userFacing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserFacingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserFacingApplication.class, args);
+	}
+
+}
---
+++ userFacing/src/test/java/com/soulstar/userFacing/UserFacingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soulstar.userFacing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserFacingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
